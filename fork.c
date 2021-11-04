297bcf4bf76dd4da34852a28204fbbc629b75f36
vigge93 <victarv@gmail.com>
2021-11-04 22:52:50 +0100

    Task 1

--- fork.c
+++ fork.c
@@ -3,16 +3,24 @@
 
 int main(int argc, char **argv)
 {
-    pid_t pid;
+    pid_t pidA;
+    pid_t pidC;
     unsigned i;
+    char letter;
     unsigned niterations = 1000;
-    pid = fork();
-    if (pid == 0) {
-        for (i = 0; i < niterations; ++i)
-            printf("A = %d, ", i);
+    pidA = fork();
+    if (pidA == 0) {
+        letter = 'A';
      } else {
-        for (i = 0; i < niterations; ++i)
-            printf("B = %d, ", i);
+        pidC = fork();
+        if (pidC == 0) {
+            letter = 'C';
+        } else {
+            printf("\nPID child process 1 (A): %d\nPID child process 2 (C): %d\n", pidA, pidC);
+            letter = 'B';
+        }
     }
+    for (i = 0; i < niterations; ++i)
+        printf("%c = %d, ", letter, i);
     printf("\n");
 }
