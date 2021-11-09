66480bfb6680563f2a67dc8fd980bd73faa8fb97
VICTOR ARVIDSSON <viar19@>
2021-11-09 08:36:01 +0000

    Task 2

--- shmem.c
+++ shmem.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
@@ -7,10 +8,13 @@
 #define SHM_R 0400
 #define SHM_W 0200
 
+
 int main(int argc, char **argv)
 {
 	struct shm_struct {
-		int buffer;
+		int buffer[10];
+		unsigned in; /* Variables to keep track of buffer */
+		unsigned out;
 		unsigned empty;
 	};
 	volatile struct shm_struct *shmp = NULL;
@@ -22,17 +26,22 @@ int main(int argc, char **argv)
 	/* allocate a chunk of shared memory */
 	shmid = shmget(IPC_PRIVATE, SHMSIZE, IPC_CREAT | SHM_R | SHM_W);
 	shmp = (struct shm_struct *) shmat(shmid, addr, 0);
-	shmp->empty = 0;
+	shmp->empty = 10;
+	shmp->in = 0;
+	shmp->out = 0;
 	pid = fork();
+
 	if (pid != 0) {
 		/* here's the parent, acting as producer */
 		while (var1 < 100) {
 			/* write to shmem */
 			var1++;
-			while (shmp->empty == 1); /* busy wait until the buffer is empty */
+			while (shmp->empty == 0); /* busy wait while the buffer is full */
 			printf("Sending %d\n", var1); fflush(stdout);
-			shmp->buffer = var1;
-			shmp->empty = 1;
+			shmp->buffer[shmp->in] = var1;
+			shmp->empty--;
+			shmp->in = (shmp->in + 1) % 10;
+			sleep((rand() % 20) / 10);
 		}
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
@@ -40,10 +49,12 @@ int main(int argc, char **argv)
 		/* here's the child, acting as consumer */
 		while (var2 < 100) {
 			/* read from shmem */
-			while (shmp->empty == 0); /* busy wait until there is something */
-			var2 = shmp->buffer;
-			shmp->empty = 0;
+			while (shmp->empty == 10); /* busy wait until there is something */
+			var2 = shmp->buffer[shmp->out];
+			shmp->empty++;
+			shmp->out = (shmp->out + 1) % 10;
 			printf("Received %d\n", var2); fflush(stdout);
+			sleep((rand() % 20) / 10);
 		}
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
